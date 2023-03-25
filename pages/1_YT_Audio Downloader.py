576cf94f051744d87649ba9fadda5eb256043a3b
NandhipatiGanesh <nandhipatiganeshkumar16@gmail.com>
2023-03-26 00:08:11 +0530

    Update 1_YT_Audio Downloader.py

--- pages/1_YT_Audio Downloader.py
+++ pages/1_YT_Audio Downloader.py
@@ -25,3 +25,32 @@ if st.button("Download Audio"):
     with open(audio_file, 'rb') as f:
         audio_bytes = f.read()
     st.download_button(label='Download Audio', data=audio_bytes, file_name='my_audio.mp3')
+    
+    #add a footer 
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: none;
+}
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://github.com/NandhipatiGanesh" target="_blank">Ganesh Kumar</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)    
